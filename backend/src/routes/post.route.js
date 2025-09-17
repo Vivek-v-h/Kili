a2fa33c0a0d67428d1521fcdc797b3bb103098d5
Vivek-v-h <vivekmusic19@gmail.com>
2025-09-17 12:35:06 +0530

    comment and notification feature added

--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -2,9 +2,12 @@ import express from 'express';
 const router = express.Router();
 import { createPost,getUserPosts,getPost, getPosts } from '../controllers/post.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
+import upload from '../middleware/upload.middleware.js';
+import { likePost } from '../controllers/post.controller.js';
+import { deletePost } from '../controllers/post.controller.js';
 
 router.get("/",getPosts)
-router,get("/:postId",getPost)
+router.get("/:postId",getPost)
 router.get("/user/:username",getUserPosts)
 
 router.post("/",protectRoute,upload.single("image"),createPost)
