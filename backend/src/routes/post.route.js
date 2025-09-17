578b2ab82621f1233cc1279bf92528f31764af68
Vivek-v-h <vivekmusic19@gmail.com>
2025-09-17 12:14:42 +0530

    post routes added

---
+++ backend/src/routes/post.route.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const router = express.Router();
+import { createPost,getUserPosts,getPost, getPosts } from '../controllers/post.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
+
+router.get("/",getPosts)
+router,get("/:postId",getPost)
+router.get("/user/:username",getUserPosts)
+
+router.post("/",protectRoute,upload.single("image"),createPost)
+router.post("/:postId/like", protectRoute, likePost);
+router.delete("/:postId", protectRoute, deletePost);
+
+export default router;
