a497272457938f6bac0f974100b06640cb0ea3aa
Vivek-v-h <vivekmusic19@gmail.com>
2025-09-17 12:48:29 +0530

    vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,6 +30,22 @@ app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-app.listen(ENV.PORT, () => {
-  console.log('Server is running on port', ENV.PORT)
-});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+  
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+    }
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
+
+
+export default app;
